fd67f18a61964b86f55fc3b09f30cbd6cd72e168
Michael Kohler <me@michaelkohler.info>
2018-09-11 19:44:21 +0200

    Support Active Recipe forwarding + minor adjustments

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 'use strict';
 
+const config = require('./lib/config');
 const sensorHandler = require('./lib/sensorHandler');
+const recipeHandler = require('./lib/recipeHandler');
 
-sensorHandler.startForwarding();
+const [
+  forwardSensorValues,
+  forwardActiveRecipes
+] = config.getActivation();
+
+if (!forwardActiveRecipes && !forwardSensorValues) {
+  throw new Error('NO_FORWARDING_ACTIVE');
+  process.exit(1);
+}
+
+if (forwardSensorValues) {
+  sensorHandler.startForwarding();
+}
+
+if (forwardActiveRecipes) {
+  recipeHandler.startForwarding();
+}
